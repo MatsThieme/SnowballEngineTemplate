e3cd8951af0f9b084df19c8764a9c32aad6f2756
Mats Thieme <mats.thieme.mt@gmail.com>
2022-01-14 19:54:16 +0100

    refactor: remove old tests

--- src/test/TestBehaviour.ts
+++
@@ -1,9 +0,0 @@
-import { Behaviour } from "SE";
-
-export abstract class TestBehaviour extends Behaviour {
-    abstract name: string;
-    abstract description: string;
-    /** approximate duration of the test in seconds */
-    duration?: number;
-    abstract test(): Promise<void> | void;
-}
