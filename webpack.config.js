54d73d057129e2fd9f87c7e8200b3beb25470dde
Mats Thieme <mats.thieme.mt@gmail.com>
2022-01-15 12:25:44 +0100

    refactor: remove ui paths

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,43 @@
-const { resolve } = require('path');
-const TerserPlugin = require('terser-webpack-plugin');
+const { resolve } = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-    entry: './src/SnowballEngine/Start.ts',
+    entry: "./src/SnowballEngine/Start.ts",
     module: {
         rules: [
             {
                 test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
-        ]
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
     },
     resolve: {
-        extensions: ['.ts', '.js'],
-        modules: ['node_modules', 'src'],
+        extensions: [".ts", ".js"],
+        modules: ["node_modules", "src"],
         alias: {
-            SE: resolve(__dirname, 'src/SnowballEngine/SnowballEngine.ts'),
-            Config: resolve(__dirname, 'SnowballEngineConfig.json'),
-            UI: resolve(__dirname, 'src/SnowballEngine/UI/'),
-            Utility: resolve(__dirname, 'src/SnowballEngine/Utilities/'),
-            Input: resolve(__dirname, 'src/SnowballEngine/Input/'),
-            Assets: resolve(__dirname, 'src/SnowballEngine/Assets/'),
-            Audio: resolve(__dirname, 'src/SnowballEngine/Audio/'),
-            GameObject: resolve(__dirname, 'src/SnowballEngine/GameObject/')
-        }
+            SE: resolve(__dirname, "src/SnowballEngine/SnowballEngine.ts"),
+            Config: resolve(__dirname, "SnowballEngineConfig.json"),
+            Utility: resolve(__dirname, "src/SnowballEngine/Utilities/"),
+            Input: resolve(__dirname, "src/SnowballEngine/Input/"),
+            Assets: resolve(__dirname, "src/SnowballEngine/Assets/"),
+            Audio: resolve(__dirname, "src/SnowballEngine/Audio/"),
+            GameObject: resolve(__dirname, "src/SnowballEngine/GameObject/"),
+        },
     },
     output: {
-        path: resolve('dist')
+        path: resolve("dist"),
     },
     optimization: {
         minimizer: [
             new TerserPlugin({
                 terserOptions: {
                     format: {
-                        comments: false
-                    }
+                        comments: false,
+                    },
                 },
-                extractComments: false
-            })
-        ]
-    }
+                extractComments: false,
+            }),
+        ],
+    },
 };
