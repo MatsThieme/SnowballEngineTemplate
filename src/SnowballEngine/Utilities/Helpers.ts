b0129a4fb6b6b0d6f7b4eac051d8d8973b91c9eb
Mats Thieme <mats.thieme.mt@gmail.com>
2022-01-23 11:54:01 +0100

    style: cleanup

--- src/SnowballEngine/Utilities/Helpers.ts
+++ src/SnowballEngine/Utilities/Helpers.ts
@@ -1,49 +1,48 @@
 import { Debug } from "../Debug";
 
 /**
- *
- * Clamps a number between min and max.
- *
+ * Clamp a number between min and max.
  */
 export const clamp = (min: number, max: number, val: number): number => Math.max(min, Math.min(max, val));
 
 /**
- *
- * Computes the linear interpolation between a and b for the parameter t.
- *
+ * Compute the linear interpolation between a and b for the parameter t.
  */
 export const lerp = (a: number, b: number, t: number): number => a + (b - a) * t;
 
 /**
- *
  * Calculate the average of numbers.
- *
  */
-export const average = (...numbers: number[]): number => numbers.reduce((t, c) => t + c, 0) / numbers.length;
+export function average(...numbers: number[]): number {
+    let sum = 0;
+
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+
+    return sum / numbers.length;
+}
 
 /**
- *
  * Execute code that may only be executed in a user event triggered context, e.g. fullscreen api or pointerlock api.
  *
  * @param cb Call on user event.
  * @param params Parameters to pass the callback on user event.
  *
  * @returns Returns Promise which resolves as result of callback.
- *
  */
 export function triggerOnUserInputEvent<T, U>(
-    cb: (...args: U[]) => T | Promise<T> = () => undefined as unknown as T,
+    cb: (...args: U[]) => T = () => undefined as unknown as T,
     ...params: U[]
 ): Promise<T> {
     return new Promise((resolve) => {
-        async function end(e: MouseEvent | KeyboardEvent | TouchEvent) {
-            if (!e.isTrusted) return;
+        const end = (event: MouseEvent | KeyboardEvent | TouchEvent): void => {
+            if (!event.isTrusted) return;
 
             try {
-                const result = await cb(...params);
-                resolve(result);
+                resolve(cb(...params));
             } catch (error) {
-                Debug.warn(error + "");
+                Debug.warn(typeof error === "object" ? JSON.stringify(error) : String(error));
             }
 
             window.removeEventListener("mousedown", end);
@@ -51,7 +50,7 @@ export function triggerOnUserInputEvent<T, U>(
             window.removeEventListener("keypress", end);
             window.removeEventListener("keyup", end);
             window.removeEventListener("touchstart", end);
-        }
+        };
 
         window.addEventListener("mousedown", end);
         window.addEventListener("mouseup", end);
@@ -62,7 +61,7 @@ export function triggerOnUserInputEvent<T, U>(
 }
 
 /**
- * Deletes properties and prototype to remove references and allow garbage collection.
+ * Remove properties and prototype to remove references and help garbage collection.
  * @param setnull if true, properties will be set to null instead of beeing deleted.
  */
 export function clearObject<T extends string | number | symbol>(
@@ -71,17 +70,18 @@ export function clearObject<T extends string | number | symbol>(
 ): void {
     Object.setPrototypeOf(object, null);
 
-    for (const key in object) {
-        if (setnull) {
+    if (setnull) {
+        for (const key in object) {
             object[key] = null;
-        } else {
+        }
+    } else {
+        for (const key in object) {
             delete object[key];
         }
     }
 }
 
 /**
- *
  * @returns Returns a number greater than or equal to min but less than max.
  *
  */
